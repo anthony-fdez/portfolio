c34d2b6077992fdf0ad72aee67231d4828766bde
Anthony <anthonyfdez23@gmail.com>
2022-05-26 23:27:10 -0400

    made the slide work with all the adds on

--- pages/protypist/index.tsx
+++ pages/protypist/index.tsx
@@ -1,6 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Swiper, SwiperSlide } from "swiper/react";
+import BackToHomeButton from "../../components/backToHomeButton/backToHomeButton";
 import styles from "./protypist.module.css";
+
+import { BsGithub } from "react-icons/bs";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper";
 
 const Protypist: NextPage = () => {
   return (
@@ -14,7 +33,93 @@ const Protypist: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <BackToHomeButton />
+        <br></br>
+        <h1>ProTypist</h1>
+        <p>
+          ProTypist is a fully featured typing game. With a multiplayer mode,
+          custom games, quotes, top most common words in 7 languages and
+          multiple themes.
+        </p>
+        <br></br>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          navigation={true}
+          pagination={{ clickable: true }}
+          spaceBetween={10}
+          slidesPerView="auto"
+        >
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/protypist/1.png"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/protypist/2.png"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/protypist/3.png"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/protypist/4.png"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/protypist/5.png"
+            />
+          </SwiperSlide>
+        </Swiper>
+        <br></br>{" "}
+        <div className={styles.modal_content_container}>
+          <h2>Technologies Used</h2>
+          <br></br>
+          <h3>Front End / App:</h3>
+          <ul>
+            <li>React</li>
+            <li>Redux</li>
+            <li>SocketIO Client</li>
+            <li>Google Analytics</li>
+          </ul>
+          <br></br>
+          <h3>Back End:</h3>
+          <ul>
+            <li>NodeJS</li>
+            <li>Express</li>
+            <li>MongoDB / Mongoose</li>
+            <li>SocketIO</li>
+            <li>Rest API</li>
+          </ul>
+          <br></br>
+          <h2>Source Code</h2>
+          <div className={styles.github_container}>
+            <BsGithub />
+            <a
+              target="_blank"
+              href="https://github.com/anthony-fdez/protypist"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </div>
+        </div>
+      </main>
 
       {/* <footer className={styles.footer}></footer> */}
     </div>
