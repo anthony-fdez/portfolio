8ee9e79da3e0c9b06ec796eb52705d285d17a545
Anthony <anthonyfdez23@gmail.com>
2022-05-26 23:36:40 -0400

    fixed super dumb bug

--- components/backToHomeButton/backToHomeButton.tsx
+++ components/backToHomeButton/backToHomeButton.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "./backToHomebutton.module.css";
+import styles from "./backToHomeButton.module.css";
 
 import { IoMdArrowBack } from "react-icons/io";
 
