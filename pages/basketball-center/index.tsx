cb1509398e1236b2bb82dbcf63c26c1a9007d778
Anthony <anthonyfdez23@gmail.com>
2022-05-26 23:14:26 -0400

    Added basketball center page

--- pages/basketball-center/index.tsx
+++ pages/basketball-center/index.tsx
@@ -1,7 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Swiper, SwiperSlide } from "swiper/react";
 import BackToHomeButton from "../../components/backToHomeButton/backToHomeButton";
 import styles from "./basketball-center.module.css";
+
+import { FaGooglePlay } from "react-icons/fa";
+import { AiFillApple } from "react-icons/ai";
+
+// Import Swiper styles
+import "swiper/css";
 
 const BasketballCenter: NextPage = () => {
   return (
@@ -17,6 +25,121 @@ const BasketballCenter: NextPage = () => {
 
       <main className={styles.main}>
         <BackToHomeButton />
+        <br></br>
+        <h1>Basketball Center</h1>
+        <p>
+          Basketball Center is a full stack application built for Android and
+          IOS, it provides stats, real time and historical information about
+          players and teams. All the NBA games stats are updated real time as
+          well. It also has a chat built in per game, allowing users to interact
+          with other people and discuss the game.
+        </p>
+        <br></br>
+        <Swiper slidesPerView="auto" spaceBetween={10}>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/basketball-center/1.png"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/basketball-center/2.png"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/basketball-center/3.png"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/basketball-center/4.png"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/basketball-center/5.png"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/basketball-center/6.png"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/basketball-center/7.png"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={styles.slide}>
+            <img
+              className={styles.image}
+              alt="Basketball Center screenshot 1"
+              src="/basketball-center/8.png"
+            />
+          </SwiperSlide>
+        </Swiper>
+        <br></br>
+        <h2>Technologies Used</h2>
+        <br></br>
+        <h3>Front End / App:</h3>
+        <ul>
+          <li>React Native for both the IOS and Android app</li>
+          <li>Redux</li>
+          <li>SocketIO Client</li>
+          <li>Google Analytics</li>
+        </ul>
+        <br></br>
+        <h3>Back End:</h3>
+        <ul>
+          <li>NodeJS</li>
+          <li>Express</li>
+          <li>MongoDB / Mongoose</li>
+          <li>SocketIO</li>
+          <li>Expo push notifications</li>
+          <li>Rest API</li>
+        </ul>
+        <br></br>
+        <h2>Download App</h2>
+        <div className={styles.download_container}>
+          <AiFillApple />
+          <a
+            target="_blank"
+            href="https://apps.apple.com/us/app/basketball-center/id1588423237"
+            rel="noreferrer"
+          >
+            View on AppStore
+          </a>
+        </div>
+        <div className={styles.download_container}>
+          <FaGooglePlay />
+          <a
+            target="_blank"
+            href="https://play.google.com/store/apps/details?id=com.basketballCenter.anthony&hl=en_US&gl=US"
+            rel="noreferrer"
+          >
+            View on Google Play Store
+          </a>
+        </div>
+        <br></br>
+        <br></br>
+        <p>
+          As of right now the source code for Basketball Center is closed source
+        </p>
       </main>
 
       {/* <footer className={styles.footer}></footer> */}
