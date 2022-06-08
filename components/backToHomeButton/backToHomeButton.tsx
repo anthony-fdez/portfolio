16efbe7148e22d3f38de37785ccdc15a0ba92310
Anthony <anthonyfdez23@gmail.com>
2022-06-07 23:44:47 -0400

    Fixed eslint

--- components/backToHomeButton/backToHomeButton.tsx
+++ components/backToHomeButton/backToHomeButton.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./backToHomeButton.module.css";
 
 import { IoMdArrowBack } from "react-icons/io";
 
 const BackToHomeButton = (): JSX.Element => {
+  const [helo, setHello] = useState(false);
+
   return (
     <div className={styles.container}>
       <Link passHref href="/">
