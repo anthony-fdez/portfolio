d8c8f7df6ede591447595c99110520cb2dcf5372
Anthony <anthonyfdez23@gmail.com>
2022-07-24 00:19:25 -0400

    Added the experience page

--- components/home/about/about.tsx
+++ components/home/about/about.tsx
@@ -9,20 +9,61 @@ const About = (): JSX.Element => {
         <h1>Abbout & Expirience.</h1>
         <br></br>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
+          I&apos;m a person that is really interested in making software, I
+          started with programming around 2019 only making little websites with
+          HTML and CSS. After a couple weeks I started learning JavaScript and I
+          was hooked. I decided that that was what I wanted to for a living
+          since I liked it that much, so I decided to major in computer science.
+        </p>
+        <br></br>
+        <h2>School</h2>
+        <p>
+          I started going to Miami Dade College in 2019, majoring in computer
+          science. Graduated in 2021 with my Associates in CS with a GPA of 3.9
         </p>
         <br></br>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam,
+          Starting 2022 I transferred to Florida International University to get
+          my Bachelors Degree. My expected graduation date is around december
+          2023.
         </p>
+        <br></br>
+        <h2>Work Experience</h2>
+        <p>
+          I got an internship at United Automobile Insurance Company doing web
+          development. At the time my internship expired I got hired as a Junior
+          Web Developer and I am still working there.
+        </p>
+        <br></br>
+        <ul>
+          <p>
+            Responsibilities I have at the job and technologies I worked with:
+          </p>
+          <li>
+            I worked on designing and developing their new{" "}
+            <a
+              className="link"
+              target="_blank"
+              href="https://uaic.net"
+              rel="noreferrer"
+            >
+              home page.{" "}
+            </a>
+            It was built from scratch using ReactJs and TypeScript.
+          </li>
+          <li>
+            I migrated several websites UIs from old and outdated JQuery to
+            ReactJS. Including sensitive websites such as the customer portal.
+          </li>
+          <li>
+            Worked on internal tools that increased the company&apos;s
+            productivity.
+          </li>
+          <li>
+            I worked integrating apis and did some backend coding as well.
+            (NodeJs and PHP)
+          </li>
+        </ul>
       </div>
     </Fade>
   );
