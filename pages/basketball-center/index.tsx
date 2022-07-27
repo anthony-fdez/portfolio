a908023dd318b0cf783d724c4855c49723add344
Anthony <anthonyfdez23@gmail.com>
2022-07-26 20:31:13 -0400

    Fixed styling and info about basketball center

--- pages/basketball-center/index.tsx
+++ pages/basketball-center/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import BackToHomeButton from "../../components/backToHomeButton/backToHomeButton";
-import styles from "./basketball-center.module.css";
+import styles from "../../styles/project.module.css";
 
 import { FaGooglePlay } from "react-icons/fa";
 import { AiFillApple } from "react-icons/ai";
@@ -37,69 +37,75 @@ const BasketballCenter: NextPage = () => {
       <Fade>
         <main className={styles.main}>
           <BackToHomeButton />
-          <br></br>
-          <h1>Basketball Center</h1>
-          <p>
-            Basketball Center is a full stack application built for Android and
-            IOS, it provides stats, real time and historical information about
-            players and teams. All the NBA games stats are updated real time as
-            well. It also has a chat built in per game, allowing users to
-            interact with other people and discuss the game.
-          </p>
-          <br></br>
+          <div className={styles.header}>
+            <h1>Basketball Center</h1>
+            <p>
+              Basketball Center is a full stack application built for Android
+              and IOS, it provides stats, real time and historical information
+              about players and teams. All the NBA games stats are updated real
+              time as well. It also has a chat built in per game, allowing users
+              to interact with other people and discuss the game.
+            </p>
+          </div>
           <ImagesCarousel
             orientation="portrait"
             images={images}
             altText={"Basketball Center Screenshot"}
           />
-          <br></br>
-          <h2>Technologies Used</h2>
-          <br></br>
-          <h3>Front End / App:</h3>
-          <ul>
-            <li>React Native for both the IOS and Android app</li>
-            <li>Redux</li>
-            <li>SocketIO Client</li>
-            <li>Google Analytics</li>
-          </ul>
-          <br></br>
-          <h3>Back End:</h3>
-          <ul>
-            <li>NodeJS</li>
-            <li>Express</li>
-            <li>MongoDB / Mongoose</li>
-            <li>SocketIO</li>
-            <li>Expo push notifications</li>
-            <li>Rest API</li>
-          </ul>
-          <br></br>
-          <h2>Download App</h2>
-          <div className={styles.download_container}>
-            <AiFillApple />
-            <a
-              target="_blank"
-              href="https://apps.apple.com/us/app/basketball-center/id1588423237"
-              rel="noreferrer"
-            >
-              View on AppStore
-            </a>
+          <div className={styles.project_info}>
+            <h2>Technologies Used</h2>
+            <h3>Front End / App:</h3>
+            <ul>
+              <li>React Native</li>
+              <li>Redux</li>
+              <li>SocketIO Client</li>
+              <li>Google Analytics</li>
+            </ul>
+            <h3>Back End:</h3>
+            <p>
+              The backed was made in NodeJS using Typescript. It handles the in
+              app chat, sending notifications to the users about their games or
+              when receiving on a chat they are active. It also runs functions
+              several times a day to update the data and stats that it gathers
+              from multiple NBA apis and scraping the web to be able to show
+              some unique and complicated tables to the user.
+            </p>
+            <ul>
+              <li>NodeJS</li>
+              <li>Express</li>
+              <li>MongoDB / Mongoose</li>
+              <li>SocketIO</li>
+              <li>Expo push notifications</li>
+              <li>Rest API</li>
+            </ul>
+            <h2>Download App</h2>
+            <div className={styles.download_container}>
+              <AiFillApple />
+              <a
+                target="_blank"
+                href="https://apps.apple.com/us/app/basketball-center/id1588423237"
+                rel="noreferrer"
+              >
+                View on AppStore
+              </a>
+            </div>
+            <div className={styles.download_container}>
+              <FaGooglePlay />
+              <a
+                target="_blank"
+                href="https://play.google.com/store/apps/details?id=com.basketballCenter.anthony&hl=en_US&gl=US"
+                rel="noreferrer"
+              >
+                View on Google Play Store
+              </a>
+            </div>
+            <br></br>
+            <br></br>
+            <p>
+              As of right now the source code for Basketball Center is closed
+              source
+            </p>
           </div>
-          <div className={styles.download_container}>
-            <FaGooglePlay />
-            <a
-              target="_blank"
-              href="https://play.google.com/store/apps/details?id=com.basketballCenter.anthony&hl=en_US&gl=US"
-              rel="noreferrer"
-            >
-              View on Google Play Store
-            </a>
-          </div>
-          <br></br>
-          <br></br>
-          <p>
-            As of right now the source code for Basketball Center is closed
-            source
-          </p>
         </main>
       </Fade>
     </div>
