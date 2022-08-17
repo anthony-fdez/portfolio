c70e0a0ffef7ba33ae5991bb0f03a8ddcb5db25d
Anthony <anthonyfdez23@gmail.com>
2022-08-17 10:27:19 -0400

    Updated the about section with new information

--- components/home/about/about.tsx
+++ components/home/about/about.tsx
@@ -3,44 +3,93 @@ import { Fade } from "react-awesome-reveal";
 import styles from "./about.module.css";
 
 const About = (): JSX.Element => {
+  const EducationTable = (): JSX.Element => {
+    // I had to make my own table using div's because for some reason
+    // I get a hidration error when using a table. This seems like a problem
+    // not fixed yet in react 18.
+
+    return (
+      <>
+        <h2 className={styles.center}>Education.</h2>
+        <div className={styles.table}>
+          <div style={{ border: "none" }} className={styles.tr}>
+            <div className={styles.first_td}>2022-01 - Present</div>
+            <div className={styles.second_td}>
+              <h3>
+                Majoring in Computer Science (Bachelor of Computer Science)
+              </h3>
+              <p>Florida International University South Campus, Miami, FL</p>
+              <br></br>
+              <p>
+                <strong>Expected Graduation:</strong> 2023
+              </p>
+            </div>
+          </div>
+          <div className={styles.tr}>
+            <div className={styles.first_td}>2019-08 - 2021-12</div>
+            <div className={styles.second_td}>
+              <h3>Major in Computer Science (Associates Degree)</h3>
+              <p>Miami Dade College Kendall Campus, Miami, FL</p>
+              <br></br>
+              <p>
+                <strong>Graduation:</strong> 2021
+              </p>
+              <p>
+                <strong>GPA:</strong> 3.9
+              </p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  const WorkTable = (): JSX.Element => {
+    return (
+      <>
+        <h2 className={styles.center}>Work Experience.</h2>
+        <div className={styles.table}>
+          <div style={{ border: "none" }} className={styles.tr}>
+            <div className={styles.first_td}>2021-10 - Present </div>
+            <div className={styles.second_td}>
+              <h3>Junior Web Developer</h3>
+              <p>United Automobile Insurance Company, Miami Gardens, FL</p>
+            </div>
+          </div>
+          <div className={styles.tr}>
+            <div className={styles.first_td}>2021-04 – 2021-10</div>
+            <div className={styles.second_td}>
+              <h3>Web Development Intern</h3>
+              <p>United Automobile Insurance Company, Miami Gardens, FL</p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
   return (
     <Fade>
       <div id="about" className={styles.about_container}>
-        <h1>Abbout & Expirience.</h1>
+        <h1 className={styles.center}>About & Experience.</h1>
         <br></br>
         <p>
-          I&apos;m a person that is really interested in making software, I
-          started with programming around 2019 only making little websites with
-          HTML and CSS. After a couple weeks I started learning JavaScript and I
-          was hooked. I decided that that was what I wanted to for a living
-          since I liked it that much, so I decided to major in computer science.
+          My biggest passion is creating software. I experienced programming for
+          the first time in 2019. It all started by making little websites with
+          HTML and CSS just for fun; however, after a couple of weeks I got more
+          invested, and I started learning JavaScript. At that point, I realized
+          that this was what I wanted to do for a living, so I decided to major
+          in Computer Science.
         </p>
         <br></br>
-        <h2>School</h2>
-        <p>
-          I started going to Miami Dade College in 2019, majoring in computer
-          science. Graduated in 2021 with my Associates in CS with a GPA of 3.9
-        </p>
+        <EducationTable />
         <br></br>
-        <p>
-          Starting 2022 I transferred to Florida International University to get
-          my Bachelors Degree. My expected graduation date is around december
-          2023.
-        </p>
+        <WorkTable />
         <br></br>
-        <h2>Work Experience</h2>
-        <p>
-          I got an internship at United Automobile Insurance Company doing web
-          development. At the time my internship expired I got hired as a Junior
-          Web Developer and I am still working there.
-        </p>
-        <br></br>
+        <h2 className={styles.center}>Key Responsibilities.</h2>
         <ul>
-          <p>
-            Responsibilities I have at the job and technologies I worked with:
-          </p>
           <li>
-            I worked on designing and developing their new{" "}
+            Design and develop a new{" "}
             <a
               className="link"
               target="_blank"
@@ -52,9 +101,15 @@ const About = (): JSX.Element => {
             It was built from scratch using ReactJs and TypeScript.
           </li>
           <li>
-            I migrated several websites UIs from old and outdated JQuery to
-            ReactJS. Including sensitive websites such as the customer portal.
+            Migrated several websites UIs from old and outdated JQuery to
+            ReactJS.
+            <ul>
+              <li>
+                This included sensitive websites such as the customer portal.
+              </li>
+            </ul>
           </li>
+          <br></br>
           <li>
             Worked on internal tools that increased the company&apos;s
             productivity.
