413cd0788064a40cc73eefc83de5c95a6b94d329
Anthony <anthonyfdez23@gmail.com>
2022-08-18 01:18:53 -0400

    Added nice animation and started a new design

--- components/home/jumbo/jumbo.tsx
+++ components/home/jumbo/jumbo.tsx
@@ -3,38 +3,49 @@ import { Fade } from "react-awesome-reveal";
 import MyButton from "../../myButton/myButton";
 import styles from "./jumbo.module.css";
 import { Parallax } from "react-scroll-parallax";
+import Wave from "react-wavify";
 
 const Jumbo = (): JSX.Element => {
   return (
-    <Fade>
-      <div className={styles.container}>
-        <Parallax className={styles.background_text_design} speed={-20}>
-          <span>DESIGN IT.</span>
-        </Parallax>
-        <Parallax className={styles.background_text_create} speed={-10}>
-          <span>DEVELOP IT.</span>
-        </Parallax>
-        <Parallax className={styles.background_text_mobile} speed={-20}>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <Parallax
+          translateX={[20, -50]}
+          className={styles.background_text_mobile}
+        >
           <span>DESIGN, DEVELOP.</span>
         </Parallax>
+        <Fade>
+          <h3>Hi, I&apos;m Anthony!</h3>
+          <h1>
+            I&apos;m a fullstack developer. Focused on building better software
+            every day.
+          </h1>
+          <MyButton>
+            <a
+              target="_blank"
+              href="mailto:anthonyfdez23@gmail.com"
+              className={styles.button}
+              rel="noreferrer"
+            >
+              Contact Me
+            </a>
+          </MyButton>
+        </Fade>
 
-        <h3>Hi, I&apos;m Anthony!</h3>
-        <h1>
-          I&apos;m a fullstack developer. Focused on building better software
-          every day.
-        </h1>
-        <MyButton>
-          <a
-            target="_blank"
-            href="mailto:anthonyfdez23@gmail.com"
-            className={styles.button}
-            rel="noreferrer"
-          >
-            Contact Me
-          </a>
-        </MyButton>
+        <Wave
+          className={styles.wave}
+          fill="white"
+          paused={false}
+          options={{
+            height: 20,
+            amplitude: 60,
+            speed: 0.15,
+            points: 3,
+          }}
+        />
       </div>
-    </Fade>
+    </div>
   );
 };
 export default Jumbo;
