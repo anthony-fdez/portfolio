230dd14c2931778b18f2fffd0e524f6eb40900a9
Anthony <anthonyfdez23@gmail.com>
2022-10-11 12:08:44 -0400

    Fixed folder structure and named custom page extensions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: [".page.tsx", "page.ts"],
+  pageExtensions: ["page.tsx", "page.ts"],
 };
 
 module.exports = nextConfig;
