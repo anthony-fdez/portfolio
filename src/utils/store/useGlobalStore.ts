347c78ca95094ae02b626b8bb1331016e490207c
Anthony Fernandez <anthonyfdez23@gmail.com>
2023-05-25 22:10:24 -0400

    Redone folder structre

--- src/utils/store/useGlobalStore.ts
+++ src/utils/store/useGlobalStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { devtools, persist } from "zustand/middleware";
+import { devtools } from "zustand/middleware";
 
 interface IProjects {
   selectedProjectComponent: JSX.Element | null;
