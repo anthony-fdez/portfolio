34e849b0a9f1672c7f646f47cadba8be8b6b956c
Anthony Fernandez <anthonyfdez23@gmail.com>
2023-06-01 00:47:06 -0400

    Clean up

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
