2838987fcc1ed23d2cfb7094d77352eb2be79c00
Anthony Fernandez <anthonyfdez23@gmail.com>
2024-09-27 11:22:43 -0400

    Remove glow and wave

--- src/components/sections/jumbo/jumbo.tsx
+++ src/components/sections/jumbo/jumbo.tsx
@@ -1,37 +1,12 @@
 import React from "react";
-import styles from "./jumbo.module.css";
-import Wave from "react-wavify";
-import AnimatedText from "../../ui/animatedText/animatedText";
-import { useScroll, motion, useTransform, useSpring } from "framer-motion";
-import useWindowDimensions from "../../../utils/hooks/useWindowDimentions";
-import { Fade } from "react-awesome-reveal";
-import useIsLoaded from "../../../utils/hooks/useIsLoader";
-import Button from "../../ui/Button/Button";
 import { Mail } from "react-feather";
+import AnimatedText from "../../ui/animatedText/animatedText";
+import Button from "../../ui/Button/Button";
+import styles from "./jumbo.module.css";
 
 const Jumbo = (): JSX.Element => {
-  const windowDimensions = useWindowDimensions();
-
-  const { scrollY } = useScroll();
-  // @ts-expect-error
-  const y = useTransform(scrollY, [0, 300], [0, -windowDimensions.height]);
-  const isLoaded = useIsLoaded();
-
-  const smoothY = useSpring(y, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
-
   return (
     <div className={styles.container}>
-      <Fade triggerOnce delay={1500} className={styles.glow_1_container}>
-        <div className={styles.glow_1} />
-      </Fade>
-      <Fade className={styles.glow_2_container}>
-        <div className={styles.glow_2} />
-      </Fade>
-      <div className={styles.glow_2} />
       <div className={styles.content}>
         <AnimatedText
           color="white"
@@ -58,32 +33,6 @@ const Jumbo = (): JSX.Element => {
           </Button>
         </a>
       </div>
-      <motion.div
-        style={{ y: smoothY }}
-        className={
-          isLoaded ? styles.wave_container_shown : styles.wave_container_hidden
-        }
-      >
-        <Wave
-          className={styles.wave}
-          fill="url(#gradient)"
-          paused={false}
-          options={{
-            height: 10,
-            amplitude: 50,
-            speed: 0.05,
-            points: 5,
-          }}
-        >
-          <defs>
-            <linearGradient id="gradient" gradientTransform="rotate(90)">
-              <stop offset="5%" stopColor="rgb(20,20,20)" />
-              <stop offset="95%" stopColor="black" />
-            </linearGradient>
-          </defs>
-        </Wave>
-        <div className={styles.wave_bg} />
-      </motion.div>
     </div>
   );
 };
